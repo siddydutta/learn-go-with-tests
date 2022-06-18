ffd872099fdeb08a2b3dc15b293873be61357c13
siddydutta <siddharthadutta@protonmail.com>
2022-06-18 10:12:17 +0530

    integers

---
+++ 1-integers/adder_test.go
@@ -0,0 +1,24 @@
+package integers
+
+import "fmt"
+import "testing"
+
+/*
+reference SO
+https://stackoverflow.com/a/72325099/8082666
+for godoc installation
+*/
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
